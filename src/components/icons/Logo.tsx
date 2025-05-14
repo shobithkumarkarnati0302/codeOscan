bcfc8f7cc3556c0fc5572f5c90698ebef7864d49
Karnati Shobith <karnatishobith78@gmail.com>
2025-05-14 11:33:51 +0000

    showing this issue

--- src/components/icons/Logo.tsx
+++ src/components/icons/Logo.tsx
@@ -5,11 +5,11 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 220 50" // New viewBox
-      width="176"      // Default width (220/50 * 40)
-      height="40"     // Default height
+      viewBox="0 0 220 50" // Consistent viewBox
+      width="176"      // Consistent default width (maintains 220/50 aspect ratio for height 40)
+      height="40"     // Consistent default height
       aria-label="CodeOscan Logo"
-      {...props}
+      {...props} // Allows overriding width/height for specific instances like on the homepage
     >
       <style>
         {`
